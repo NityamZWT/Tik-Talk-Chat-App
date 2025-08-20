c24788f0fced2f9473effa7d8af93b8e4d5249a8
nityamZWT <nityam@zealousweb.com>
2025-08-20 20:18:05 +0530

    Nityam: implement registration form

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "Tik Talk - Chat App",
   description: "A Chat App build with Next.js",
+  icons: {
+    icon: "/favicon.png",        // default
+    // shortcut: "/favicon-16x16.png",
+    // apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,13 +1,32 @@
 // import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <Button>Click Me</Button>
-    </div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-slate-50 dark:bg-slate-900 bg-radial-[at_50%_90%] from-slate-100 to-slate-500 dark:from-slate-800 dark:to-slate-900">
+        <div className="flex flex-col items-center justify-center p-4 w-full max-w-[700px]">
+          <div className="text-center mb-8">
+            <h1 className="text-5xl font-extrabold text-shadow-lg text-shadow-gray-400 dark:text-shadow-none text text-gray-600">Welcome to the Chat App</h1>
+            <p className="mt-2 text-lg font-medium opacity-50">Connect with your friends and start chatting!</p>
+          </div>
+          <div className="flex flex-row item-center justify-center w-full gap-4">
+            <Button size={"lg"} className={"cursor-pointer"}>
+              <Link href={"/signup"}>
+                Sign up
+              </Link>
+            </Button>
+            <Button size={"lg"} className={"cursor-pointer"}>
+              <Link href={"/login"}>
+                Login  
+              </Link>
+            </Button>
+          </div>
+        </div>
+
+      </div>
     </>
   );
 }
